b12e76f937eac7ba80a9a3946dd1c7129d12c197
725moimoi-dev <725moimoi@gmail.com>
2025-08-31 15:12:32 +0900

    coffee step5

--- ChallengeProjects/NetworkedApp/NetworkedApp/MainTabView.swift
+++ ChallengeProjects/NetworkedApp/NetworkedApp/MainTabView.swift
@@ -1,11 +1,25 @@
 import SwiftUI
 
 struct MainTabView: View {
-
+    
     var body: some View {
-        Text("MainTabView")
+        TabView{
+            CoffeeListView()
+                .tabItem{
+                    Image(systemName: "list.bullet")
+                    Text("Coffes")
+                }
+            
+            Text ("Favorites View")
+                .tabItem{
+                    Image(systemName: "star.fill")
+                    Text("Favorites")
+                }
+            
+        }
     }
 }
+
 
 #Preview {
     MainTabView()
