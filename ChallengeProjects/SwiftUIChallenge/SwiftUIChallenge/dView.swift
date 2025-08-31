54aa2cc402610a3f4b5bd90518f1c59664ef7323
725moimoi-dev <725moimoi@gmail.com>
2025-08-31 12:02:32 +0900

    challenge

--- ChallengeProjects/SwiftUIChallenge/SwiftUIChallenge/dView.swift
+++ ChallengeProjects/SwiftUIChallenge/SwiftUIChallenge/dView.swift
@@ -9,9 +9,29 @@ import SwiftUI
 
 struct dView: View {
     var body: some View {
-        Text("D. 四角形の中に3つのテキストを等間隔に配置（異なるアライメント）")
+        Rectangle()
+                .fill(Color.red)
+                .frame(width: 300, height: 150)
+                .overlay(
+            VStack{
+                Text("Left Aligned")
+                    .foregroundStyle(.white)
+                    .frame(maxWidth: .infinity,
+                           maxHeight: .infinity,
+                           alignment:.leading)
+                Text("Center Aligned")
+                    .foregroundStyle(.white)
+                    .font(.title)
+                    .frame(maxWidth: .infinity, alignment:.center)
+                Text("Right Aligned")
+                    .foregroundStyle(.white)
+                    .frame(maxWidth: .infinity,
+                           maxHeight: .infinity,
+                           alignment:.trailing)
+            }
+        )}
     }
-}
+
 
 #Preview {
     dView()
